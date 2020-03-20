f54ac3859b5844842311e6a147efa2dec1887673
Marcos Pinardi <marcospinardi@gmail.com>
2020-03-20 19:15:30 -0300

    adminCategorias

--- sistema/funciones/categorias.php
+++ sistema/funciones/categorias.php
@@ -3,3 +3,12 @@
 #############################
 ##### CRUD DE CATEGORIAS ####
 #############################
+
+    function listarCategorias()
+    {
+            $link = conectar();
+            $sql = "SELECT idCategoria, catNombre
+                        FROM categorias";
+            $resultado = mysqli_query($link, $sql);
+            return $resultado;
+    }
