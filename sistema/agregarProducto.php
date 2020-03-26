187fb98197fd0c3d4cfc0ab9b74a2d9ce7e9cf57
marcos pinardi <exegeses@gmail.com>
2020-03-25 21:59:44 -0300

    agregar producto

--- sistema/agregarProducto.php
+++ sistema/agregarProducto.php
@@ -1,11 +1,25 @@
-<?php  
+<?php
+
+    require 'funciones/conexion.php';
+    require 'funciones/productos.php';
+    $chequeo = agregarProducto();
 	include 'includes/header.html';
 	include 'includes/nav.php';  
 ?>
 
     <main class="container">
-        <h1>Tema de la página</h1>
-
+        <h1>Alta de un nuevo Producto</h1>
+<?php
+        $clase = 'danger';
+        $mensaje = 'No se puedo agregar el productos.';
+        if ( $chequeo ){
+            $clase = 'success';
+            $mensaje = 'Productos agregado correctamente.';
+        }
+?>
+        <div class="alert alert-<?= $clase ?>">
+            <?= $mensaje ?>
+        </div>
     </main>
 
 <?php  include 'includes/footer.php';  ?>
--- subirArchivos/procesa.php
+++ subirArchivos/procesa.php
@@ -5,10 +5,14 @@
 <pre><?php print_r($prdImagen) ?></pre>
 <?php
 
-    $ruta = 'fotos/';
-    $prdImagen = $_FILES['prdImagen']['name'];
-    $temp = $_FILES['prdImagen']['tmp_name'];
+    function subirArchivo()
+    {
+        $ruta = 'fotos/';
+        $prdImagen = $_FILES['prdImagen']['name'];
+        $temp = $_FILES['prdImagen']['tmp_name'];
 
-    move_uploaded_file( $temp, $ruta.$prdImagen);
+        move_uploaded_file( $temp, $ruta.$prdImagen);
+    }
+
+    subirArchivo();
 ?>
-
