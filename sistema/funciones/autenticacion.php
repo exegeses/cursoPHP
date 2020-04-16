df4a7e5e579ef221827031e44bc215c854078e1e
marcos pinardi <exegeses@gmail.com>
2020-04-15 21:55:00 -0300

    autenticacion + logout

--- sistema/funciones/autenticacion.php
+++ sistema/funciones/autenticacion.php
@@ -25,6 +25,12 @@
             ######## autenticación con sesiones
             //session_start() está en config.php
             $_SESSION['login'] = 1;
+
+            //guardamos nombre y apellido en sesión
+            $datos = mysqli_fetch_assoc($resultado);
+            $_SESSION['usuNombre'] = $datos['usuNombre'];
+            $_SESSION['usuApellido'] = $datos['usuApellido'];
+
             // redirección a admin
             header( 'location: admin.php' );
         }
@@ -32,7 +38,10 @@
 
     function logout()
     {
-
+        session_unset();
+        session_destroy();
+        //redirección a index (con delay)
+        header('refresh:3; url=index.php');
     }
 
     function autenticar()
